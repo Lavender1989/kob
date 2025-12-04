9b6eba2831ec85aae4d617821263d29c599b35f0
Qingrui Wang <wqr20011989@163.com>
2025-12-04 21:54:37 +0800

    实现了对战的统一操作（设计输赢界面以及再来一局的正确跳转，将游戏的逻辑放在后端），创建记录对战表record

---
+++ backend/src/main/java/com/kob/backend/consumer/utils/Player.java
@@ -0,0 +1,49 @@
+package com.kob.backend.consumer.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Player {
+    private Integer id;
+    private Integer sx;
+    private Integer sy;
+    private List<Integer> steps;  // 每名玩家每一步的方向
+
+    private boolean check_tail_increasing(int step) {
+        // 检查当前回合，蛇的长度是否增加
+        if (step <= 10) return true;
+        return step % 3 == 1;
+    }
+
+    public List<Cell> getCells() {
+        List<Cell> res = new ArrayList<>();
+        int[] dx = {-1, 0, 1, 0}, dy = {0, 1, 0, -1};
+        int x = sx, y = sy;
+        res.add(new Cell(x, y));
+        int step = 0;
+        for (int d: steps) {
+            x += dx[d];
+            y += dy[d];
+            res.add(new Cell(x, y));
+            if(!check_tail_increasing( ++ step)) {
+                res.remove(0); // 把蛇尾删掉
+            }
+        }
+        return res;
+    }
+
+    public String getStepString() {
+        StringBuilder res = new StringBuilder();
+        for (int d:steps) {
+            res.append(d);
+        }
+        return res.toString();
+    }
+}
--- web/src/store/pk.js
+++ web/src/store/pk.js
@@ -5,6 +5,14 @@ export default {
     opponent_username: "", // 存储对手的用户名
     opponent_photo: "",
     gamemap: null, // 存储对战地图
+    a_id: 0, // 存储自己的id
+    a_sx: 0,
+    a_sy: 0,
+    b_id: 0, // 存储对手的id
+    b_sx: 0,
+    b_sy: 0,
+    gameObject: null, // 存储对战地图的对象
+    loser: "none", // none, all, A, B
   },
   getters: {
   },
@@ -19,10 +27,21 @@ export default {
     updateStatus(state, status) {
       state.status = status;
     },
-    updateGamemap(state, gamemap) {
-      state.gamemap = gamemap;
+    updateGame(state, game) {
+      state.gamemap = game.map;
+      state.a_id = game.a_id;
+      state.a_sx = game.a_sx;
+      state.a_sy = game.a_sy;
+      state.b_id = game.b_id;
+      state.b_sx = game.b_sx;
+      state.b_sy = game.b_sy;
+    },
+    updateGameObject(state, gameObject) {
+      state.gameObject = gameObject;
+    },
+    updateLoser(state, loser) {
+      state.loser = loser;
     }
-
   },
   actions: {
   },
